e2c61a85a7cd351de1387f5c3ed21206ba019893
Lei Zhilong <leizhilong@me.com>
2013-07-12 19:31:53 +0800

    skip test

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 git pull --rebase
-mvn clean package
+mvn clean package -Dmaven.test.skip=true
 scp /home/leizhilong/git/YCSB/distribution/target/ycsb-0.1.4.tar.gz infinispan@172.17.254.230:/home/infinispan
