e035e3f152d27dd4c6902ec60922d46e2ef515e7
Lei Zhilong <leizhilong@me.com>
2013-07-18 10:11:37 +0800

    add script

--- myscript/runJM.sh
+++ myscript/runJM.sh
@@ -1,3 +1,38 @@
 export TS=`date +%Y%m%d-%H.%M.%S`
+export NUM=2
+jdg-mem.sh
+
+sleep 1
+export TS=`date +%Y%m%d-%H.%M.%S`
+export NUM=25
+jdg-mem.sh
+
+
+sleep 1
+export TS=`date +%Y%m%d-%H.%M.%S`
 export NUM=50
-. jdg-mem.sh
+jdg-mem.sh
+
+
+sleep 1
+export TS=`date +%Y%m%d-%H.%M.%S`
+export NUM=75
+jdg-mem.sh
+
+
+sleep 1
+export TS=`date +%Y%m%d-%H.%M.%S`
+export NUM=100
+jdg-mem.sh
+
+
+sleep 1
+export TS=`date +%Y%m%d-%H.%M.%S`
+export NUM=150
+jdg-mem.sh
+
+
+sleep 3
+export TS=`date +%Y%m%d-%H.%M.%S`
+export NUM=200
+jdg-mem.sh
--- myscript/runMemcached.sh
+++ myscript/runMemcached.sh
@@ -1,38 +1,38 @@
 export TS=`date +%Y%m%d-%H.%M.%S`
 export NUM=2
-redis.sh
+memcached.sh
 
 sleep 1
 export TS=`date +%Y%m%d-%H.%M.%S`
 export NUM=25
-redis.sh
+memcached.sh
 
 
 sleep 1
 export TS=`date +%Y%m%d-%H.%M.%S`
 export NUM=50
-redis.sh
+memcached.sh
 
 
 sleep 1
 export TS=`date +%Y%m%d-%H.%M.%S`
 export NUM=75
-redis.sh
+memcached.sh
 
 
 sleep 1
 export TS=`date +%Y%m%d-%H.%M.%S`
 export NUM=100
-redis.sh
+memcached.sh
 
 
 sleep 1
 export TS=`date +%Y%m%d-%H.%M.%S`
 export NUM=150
-redis.sh
+memcached.sh
 
 
-sleep 1
+sleep 3
 export TS=`date +%Y%m%d-%H.%M.%S`
 export NUM=200
-redis.sh
+memcached.sh
